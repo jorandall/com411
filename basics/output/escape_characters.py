26d3aa2f277c0ff5c84fd798060a8f63b5e162d8
repl.it user <replituser@example.com>
2020-10-02 12:00:11 +0000

    Added code to display escape characters.

---
+++ basics/output/escape_characters.py
@@ -0,0 +1,6 @@
+# Display escape characters
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a back slack")
+print("\" Displays a double quote")
+print("\' Displays a sinlge quote")
