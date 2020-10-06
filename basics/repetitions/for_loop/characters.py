06070333dfe93b0f0936d6ec2e06dccdd76df19a
repl.it user <replituser@example.com>
2020-10-06 11:28:46 +0000

    Added code to demonstrate the use of for loops in string indexing.

---
+++ basics/repetitions/for_loop/characters.py
@@ -0,0 +1,11 @@
+# Ask the user to define which markings they've seen
+print("What strange markings do you see?")
+markings = input()
+
+# Displaying the characters
+print("\nIdentifying...\n")
+
+for count in range(0, len(markings), 1):
+  print("Index", count, ":", markings[count])
+
+print("\nDone!")
