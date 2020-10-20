18fdec47af5793cdc0675cd5fa59dbab8766f1d8
repl.it user <replituser@example.com>
2020-10-20 08:58:40 +0000

    Added code to demonstrate a simple list.

---
+++ data/lists/simple_list.py
@@ -0,0 +1,9 @@
+# Define function to return directions
+def get_directions():
+  directions = ["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
+  return directions
+
+def run():
+  print(get_directions())
+
+run()
