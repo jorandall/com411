a8b743a32ce991b5b32df7bfd9b233436634cec0
repl.it user <replituser@example.com>
2020-11-03 14:42:58 +0000

    Added code to generate csv.

--- data/files/text/generate.py
+++ data/files/text/generate.py
@@ -4,13 +4,19 @@ import csv
 def search(file_name):
   print("Searching...", end="")
   data = {}
+  section_name = ""
 
   with open(file_name) as csvfile:
     for line in csvfile:
       if line.startswith("Section"):
         section = line.split(":")
+        section_name = section[1].strip()
       else:
-        data.append({section: line[:-1]})
+        if (section_name in data):  # Check if already exists, if so add to the end
+          values = data[section_name]
+          values.append(line.strip())
+        else:
+          data[section_name] = [line.strip()] # If doesn't exist, add to the beginning
   
   print("Done!")
   return data
@@ -20,6 +26,16 @@ def search(file_name):
 # Define the run function and write the new document
 def run():
   data = search("data/files/text/books.txt")
-  csv.write("data/files/text/generated.csv", data)
+
+  with open ("data/files/text/generated.csv", "w") as file:
+
+    for item in data.items():
+      section = item[0]
+      books = item[1]
+
+      for book in books:
+        file.write(f"{section}, {book}\n")
+
+
 
 run()
