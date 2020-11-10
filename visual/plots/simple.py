6c0fd8c49ac92566ba970bc49be6d0c9d981da3f
repl.it user <replituser@example.com>
2020-11-10 10:46:42 +0000

    Added code to display multiple plots.

--- visual/plots/simple.py
+++ visual/plots/simple.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 
+# Define function to display graph
 def display(x, y):
   plt.plot(x,y)
   plt.show()
 
+# Define function to run program
 def run():
   x_values = [1, 2, 3, 4, 5]
   y_values = [1, 4, 9, 16, 25]
