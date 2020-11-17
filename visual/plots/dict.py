68d51e31aacc51cfcdaf69d20a94bfb6b0c67115
repl.it user <replituser@example.com>
2020-11-17 08:48:48 +0000

    Updated code to demonstrate plotting a graph with a dict.

--- visual/plots/dict.py
+++ visual/plots/dict.py
@@ -1,51 +1,46 @@
 import matplotlib.pyplot as plt
+import random as rnd
 
+# Define the data function to ask user how lines should be displayed
 def data():
   paths = {}
-  print("What type of line would you like (dotted, dashed or solid)?")
-  line = input()
-  print("What colour would you like (red, green or blue)?")
+
+  # Ask user for line details
+  print("What type of line would you like (:, -- or -)?")
+  line_style = input()
+  print("What colour would you like (r, g or b)?")
   colour = input()
-  print("What style of marker would you like (circle, square or triangle?")
-  shape = input()
-  
-  if line == "dotted":
-    paths[line] = ":"
-  elif line == "dashed":
-    paths[line] = "--"
-  elif line == "solid":
-    paths[line] = "-"
-    
-  if colour == "red":
-    paths[colour] = "r"
-  elif colour == "green":
-    paths[colour] = "g"
-  elif colour == "blue":
-    paths[colour] = "b"
-    
-  if shape == "circle":
-    paths[shape] = "o"
-  elif shape == "square":
-    paths[shape] = "s"
-  elif shape == "triangle":
-    paths[shape] = "^"
+  print("What style of marker would you like (o, s or ^?")
+  marker_style = input()
+
+  # Update dictionary with user responses
+  paths["line_style"] = line_style
+  paths["colour"] = colour
+  paths["marker_style"] = marker_style
   
   return paths
-  
+
+# Define the generate function to ask user for how many lines
 def generate():
   print("How many lines would you like to display?")
-  lines = input()
+  num_lines = int(input())
   
-  for line in lines:
-    data_dict = data()
-    plt.plot(line, data_dict[1])
-    plt.show()
-  
+  for num_line in range(num_lines):
+    values = data()
+    x = [0, rnd.randrange(1, 10)]
+    y = [0, rnd.randrange(1, 10)]
+    plt.plot(x, y, "{}{}{}".format(values["line_style"], values["colour"], values["marker_style"]))
+
+  plt.show()
+
+# Define run function
 def run():
   print("Running...")
   generate()
+
   print("Done!")
-  
+
+# Function call for run
 run()
     
     
