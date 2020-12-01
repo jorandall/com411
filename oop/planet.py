13ac56a847f6718decf5bd2478309126a34d9914
repl.it user <replituser@example.com>
2020-12-01 11:03:57 +0000

    Updated planet class to use a dictionary.

--- oop/planet.py
+++ oop/planet.py
@@ -1,32 +1,35 @@
 # Define Planet class
 class Planet:
   # Instance variables
-  humans = []
-  robots = []
+  inhabitants = {
+  "humans": [],
+  "robots": []
+  }
 
   # Instance method for adding a human 
   def add_human(self, name):
-    Planet.humans.append(name)
+    Planet.inhabitants["humans"].append(name)
 
   # Instance method for removing a human 
   def remove_human(self, name):
-    Planet.humans.remove(name)
+    Planet.inhabitants["humans"].remove(name)
 
   # Instance method for adding a robot 
   def add_robot(self, name):
-    Planet.robots.append(name)
+    Planet.inhabitants["robots"].append(name)
 
   # Instance method for removing a robot
   def remove_robot(self, name):
-    Planet.robots.remove(name)
+    Planet.inhabitants["robots"].remove(name)
 
-# Magic methods fo
+# Magic methods for displaying data
   def __repr__(self):
-    return f"humans = {Planet.humans}, robots = {Planet.robots}"
+    return "humans = {}, robots = {}".format(Planet.inhabitants["humans"], Planet.inhabitants["robots"])
 
   def __str__(self):
-    return f"The humans on this planet are {Planet.humans}.\nThe robots on this planet are {Planet.robots}"
+    return "The humans on this planet are {}\nThe robots on this planet are {}".format(str(Planet.inhabitants["humans"])[1:-1].replace("'",""), str(Planet.inhabitants["robots"])[1:-1].replace("'", ""))
 
+# Testing area
 planet = Planet()
 planet.add_human("Darren")
 planet.add_robot("Beep")
