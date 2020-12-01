902b40bf148e80d474eb691810d2930a07e3c001
repl.it user <replituser@example.com>
2020-12-01 12:57:29 +0000

    Updated universe class.

--- oop/universe.py
+++ oop/universe.py
@@ -1,27 +1,40 @@
 import random as rnd
 from planet import Planet
+from robot import Robot
+from human import Human
 
 
 class Universe:
   def __init__(self):
     self.planets = []
-  
-  def generate(self, new_planet):
 
-    for iterations in range(5):
-      humans = rnd.randrange(1, 100)
-      new_planet.add_human(humans)
-      robots = rnd.randrange(1, 100)
-      new_planet.add_robot(robots)
-
-    self.planets.append(new_planet)
+  def __repr__(self):
+    return f"universe(planets={self.planets}"
 
   def __str__(self):
-    return f"The planets in this universe are {self.planets}"
+    return f"The universe contains {len(self.planets)} planets."
+  
+  def generate(self):
+    # Create a new planet
+    planet = Planet()
 
-mars = Planet()
-universe = Universe()
-universe.generate(mars)
-venus = Planet()
-universe.generate(venus)
-print(universe)
+    # Populate with random humans and robots
+    for index in range(rnd.randint(1, 10)):
+      robot = Robot(f"Robot{index}")
+      planet.add_robot(robot)
+    
+    for index in range(rnd.randint(1, 10)):
+      human = Human(f"Human{index}")
+      planet.add_human(human)
+
+    self.planets.append(planet)
+
+
+
+if (__name__ == "__main__"):
+  universe = Universe()
+  universe.generate()
+  universe.generate()
+  print(universe)
+
+
